74f8af38816f52b81d737efb6295d3e28361f681
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-02-23 19:08:22 +0530

    Create Prime Numbers.cpp

---
+++ Prime Numbers.cpp
@@ -0,0 +1,19 @@
+vector<int> Solution::sieve(int n) {
+    vector prime(n,false);
+    vector <int> ans;
+    if(n<2){
+        
+    }
+    
+    for(int i = 2 ; i*i < n ; i++){
+        for(int j = i*i ; j < n ; j += i){
+            prime[j] = true;
+        }
+    }
+    for(int i = 2 ; i < n ; i++){
+        if(prime[i] == false)
+            ans.push_back(i);
+    }
+    return ans;
+    
+}
